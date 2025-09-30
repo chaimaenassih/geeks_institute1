c5fb80840bdc6ca534a0a9269987f1c80fd5f85f
Nassih Chaimae <nassihchaimae30@gmail.com>
2025-09-30 12:37:50 +0200

    Suppression définitive des fichiers

--- week_2/day_5/Car-Dealership-project/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# models/__init__.py
-from .vehicles import vehicles_bp
-from .sales import sales_bp
-from .customers import customers_bp
-from .salespeople import salespeople_bp
-
-__all__ = ["vehicles_bp", "sales_bp", "customers_bp", "salespeople_bp"]
